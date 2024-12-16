106494ae487248ddb27faf53516bce62caabd785
PanthitaKKU <panthita.sa@kkumail.com>
2024-12-16 16:30:55 +0700

    edit footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="p-6 flex gap-9 text-black border-t-2 border-gray-400">
     <!-- logo n ad -->
     <div class="ml-7">
-      <img src="/public/image/logo.png" class="w-[120px] flex" />
+      <img src="/public/image/logo.png" class="w-[150px] flex" />
 
       <p class="mt-4 ml-4 text-sm">
         88/8 หมู่ 12 ตำบลศิลา อำเภอเมือง จังหวัดขอนแก่น 40000
