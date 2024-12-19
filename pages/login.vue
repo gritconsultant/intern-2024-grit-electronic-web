0bd0c026d6e263c9da46ac18f8befb0e7f794f71
PanthitaKKU <panthita.sa@kkumail.com>
2024-12-19 14:50:32 +0700

    update login

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="h-screen bg-[url('/images/Loginbg.jpg')] bg-cover bg-center flex justify-center items-center">
         <div class="w-[450px] h-[600px] bg-black/50 rounded-2xl">
-            <div class="w-[300px] h-[600px]">
-                <img src="public/images/logologin.png" class="">
+            <div>
+                <img src="">
             </div>
         </div>
     </div>
